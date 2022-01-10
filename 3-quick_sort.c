f77ec594619230bc43a30ad928d9460fe53af52b
roveld99 <roveld99@gmail.com>
2022-01-10 21:11:05 +0300

    sorting_algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,71 +1,66 @@
 #include "sort.h"
 /**
-  * quick_sort - quicksort algorithm
-  * @array: array to be sorted
-  * @size: size of array
-  */
+ * quick_sort_rec - sorts an array of integers in ascending order using
+ * the Selection sort algorithm using recursion
+ * @array: pointer to array
+ * @size: size of the array
+ * @array_init: initial pointer to array
+ * @size_init: initial size of the array
+ **/
+void quick_sort_rec(int *array_init, size_t size_init, int *array, size_t size)
+{
+	size_t j, aux;
+	int aux2;
+	int i = -1, n, pivot = array[size - 1];
+
+	if (array && size > 1)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] < pivot)
+			{
+				i++;
+				aux = i;
+				if (aux != j && array[j] != array[aux])
+				{
+					n = array[j];
+					array[j] = array[i];
+					array[i] = n;
+					print_array(array_init, size_init);
+				}
+			}
+
+
+		}
+		aux2 = size;
+		if (aux2 - 1 != i + 1 && array[aux2 - 1] != array[i + 1])
+		{
+			array[size - 1] = array[i + 1];
+			array[i + 1] = pivot;
+			print_array(array_init, size_init);
+		}
+		if (i > 0)
+		{
+			quick_sort_rec(array_init, size_init, array, i + 1);
+		}
+		quick_sort_rec(array_init, size_init, array + i + 2,  size - (i + 2));
+	}
+}
+/**
+ * quick_sort - sorts an array of integers in ascending order using
+ * the Selection sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
+**/
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size <= 1)
-		return;
-	sort_alg(array, 0, size - 1, size);
-}
+	int *array_init;
+	size_t size_init;
 
-/**
-  * sort_alg - recursive sorting algorithm
-  * @arr: array
-  * @left: leftmost index
-  * @right: rightmost index
-  * @size: full size of array
-  */
-void sort_alg(int *arr, int left, int right, size_t size)
-{
-	int pivot;
-
-	if (left < right)
+	if (array)
 	{
-		pivot = split(arr, left, right, size);
-		sort_alg(arr, left, pivot - 1, size);
-		sort_alg(arr, pivot + 1, right, size);
+		array_init = array;
+		size_init = size;
+		quick_sort_rec(array_init, size_init, array, size);
 	}
-}
-
-/**
-  * split - split array
-  * @arr: array
-  * @left: leftmost index
-  * @right: rightmost index
-  * @size: full array size
-  * Return: pivot index
-  */
-int split(int *arr, int left, int right, size_t size)
-{
-	int i, i2, pivot, tmp;
-
-	pivot = arr[right];
-	i = left;
-
-	for (i2 = left; i2 < right; i2++)
-	{
-		if (arr[i2] < pivot)
-		{
-			if (i != i2)
-			{
-				tmp = arr[i2];
-				arr[i2] = arr[i];
-				arr[i] = tmp;
-				print_array(arr, size);
-			}
-			i++;
-		}
-	}
-	if (arr[i] != arr[right])
-	{
-		tmp = arr[i];
-		arr[i] = arr[right];
-		arr[right] = tmp;
-		print_array(arr, size);
-	}
-
-	return (i);
 }
